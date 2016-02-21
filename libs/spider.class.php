8cda4f30914d88d490f4a84a831c071f72d96975
Jordan Schelew <jas8522@gmail.com>
2016-02-20 21:32:45 -0600

    Change static function call to object notation

--- libs/spider.class.php
+++ libs/spider.class.php
@@ -436,7 +436,7 @@ class Spider{
 
 	// function to check whether link is a redirect
 	function isLinkRedirect($url) {
-			$header = Spider::getHeader($url);
+			$header = $this->getHeader($url);
 			if (stristr($header, '301 Moved Permanently') || stristr($header, '308 Permanent Redirect')) {
 					return true;
 			} else {
