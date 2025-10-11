3e0ba2ef9162fd45022a4d8d6df72ee4cbf57faa
Geo Varghese <sendtogeo@gmail.com>
2025-10-11 23:49:26 +0530

    bootstrap changes

--- themes/classic/views/directory/generatesubmission.ctp.php
+++ themes/classic/views/directory/generatesubmission.ctp.php
@@ -7,24 +7,19 @@
 	</tr>
 	<?php
 	$colCount = 4;
-	if(count($list) > 0){
-		$catCount = count($list);
-		$i = 0;
-		foreach($list as $listInfo){
-
-			$class = ($i % 2) ? "blue_row" : "white_row";
-			$confirm = empty($listInfo['status']) ? $spText['common']["No"] : $spText['common']["Yes"];
+	if(count($list) > 0) {
+		foreach($list as $listInfo) {
+			$confirm = showStatusBadge($listInfo['status'], "yesno");
 			$statusId = "status_".$listInfo['id'];
 			$checkStatusLink = "<script>scriptDoLoad('directories.php', '$statusId', 'sec=checkstatus&id={$listInfo['id']}');</script>";
 			?>
-			<tr class="<?php echo $class?>">
+			<tr>
 				<td><?php echo $listInfo['domain']?></td>
 				<td><?php echo date('Y-m-d', $listInfo['submit_time']); ?></td>
-				<td><?php echo $confirm?></td>
+				<td class="text-center"><?php echo $confirm?></td>
 				<td id="<?php echo $statusId?>"><?php echo $checkStatusLink?></td>
 			</tr>
 			<?php
-			$i++;
 		}
 	}else{
 		echo showNoRecordsList($colCount-2);
