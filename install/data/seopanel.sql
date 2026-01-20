a2979c5542c9fc0059cc151558b9d24d5ee4adb4
sendtogeo <sendtogeo@gmail.com>
2026-01-20 12:37:45 +0530

    SP 5.1.0 changes - quick fixes

--- install/data/seopanel.sql
+++ install/data/seopanel.sql
@@ -1548,22 +1548,6 @@ UPDATE searchengines SET url = REPLACE(url, '&num=[--num--]', '') WHERE  url LIK
 
 UPDATE searchengines SET url = REPLACE(url, '&as_qdr=all&gws_rd=cr&nfpr=1', '') WHERE  url LIKE '%google%';
 
-ALTER TABLE `rankresults` ADD `spam_score` FLOAT NOT NULL DEFAULT '0' AFTER `moz_rank`;
-
-ALTER TABLE `rankresults` CHANGE `moz_rank` `moz_rank` FLOAT NOT NULL DEFAULT '0';
-ALTER TABLE `rankresults` CHANGE `domain_authority` `domain_authority` FLOAT NOT NULL DEFAULT '0',
-CHANGE `page_authority` `page_authority` FLOAT NOT NULL DEFAULT '0';
-
-ALTER TABLE `directories` ADD `spam_score` FLOAT NOT NULL DEFAULT '0' AFTER `pagerank`;
-
-ALTER TABLE `auditorreports` ADD `spam_score` FLOAT NOT NULL DEFAULT '0' AFTER `page_authority`;
-
-ALTER TABLE `backlinkresults` ADD `external_pages_to_page` INT NOT NULL DEFAULT '0' AFTER `alexa`;
-
-ALTER TABLE `backlinkresults` ADD `external_pages_to_root_domain` INT NOT NULL DEFAULT '0' AFTER `external_pages_to_page`;
-
-ALTER TABLE `backlinkresults` CHANGE `google` `google` INT NOT NULL DEFAULT '0', CHANGE `msn` `msn` INT NOT NULL DEFAULT '0';
-
 update `settings` set display=0 WHERE `set_name` LIKE 'SP_MOZ_API_ACCESS_ID';
 
 -- Add setting to use sample API data (saves API credits during development/testing)
